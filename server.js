298c8ccf339ae872a5a1f3fdba5395bd32b5f702
Nitin Kumar <nitinkumar6912@gmail.com>
2021-02-11 02:53:09 +0530

    update for heroku

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(express.static(__dirname + '/dist/swapsoul-frontend'));
 // Start the app by listening on the default
 // Heroku port
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/frontend/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/swapsoul-frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, '0.0.0.0');
