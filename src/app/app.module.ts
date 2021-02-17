3ff6b17c5d8ebd80bef209674f728c00135ba2c7
SUSHMADASCJ <51118772+SUSHMADASCJ@users.noreply.github.com>
2021-02-17 12:56:07 +0530

    updates on responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BodyUpperComponent } from './body-upper/body-upper.component';
-import { CollectionComponent } from './collection/collection.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { BannerComponent } from './banner/banner.component';
@@ -14,20 +13,21 @@ import { FooterComponent } from './footer/footer.component';
 import { StoryComponent } from './story/story.component';
 import {HttpClientModule} from '@angular/common/http';
 import { CorouselComponent } from './corousel/corousel.component';
-import { GenderComponent } from './gender/gender.component'
+import { GenderComponent } from './gender/gender.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BodyUpperComponent,
-    CollectionComponent,
     BannerComponent,
     ContactComponent,
     FooterComponent,
     StoryComponent,
     CorouselComponent,
     GenderComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/corousel/corousel.component.ts
+++ src/app/corousel/corousel.component.ts
@@ -11,5 +11,4 @@ export class CorouselComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
