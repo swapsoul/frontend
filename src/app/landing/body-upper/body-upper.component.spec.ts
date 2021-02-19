b619a6fa5e1fccb9b293bea9577af2b0594815ee
phoenixnitin <nitinkumar6912@gmail.com>
2021-02-19 21:31:32 +0530

    Dependency Update

--- src/app/landing/body-upper/body-upper.component.spec.ts
+++ src/app/landing/body-upper/body-upper.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BodyUpperComponent } from './body-upper.component';
 
@@ -6,7 +6,7 @@ describe('BodyUpperComponent', () => {
   let component: BodyUpperComponent;
   let fixture: ComponentFixture<BodyUpperComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BodyUpperComponent ]
     })
