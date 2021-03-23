d795bcf869cf4821fbffd17d3d6c376aa32aa015
phoenixnitin <nitinkumar6912@gmail.com>
2021-03-23 19:30:42 +0530

    Add: google analytics

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://swapsoul-backend-prd.herokuapp.com/api/'
+  baseUrl: 'https://swapsoul-backend-prd.herokuapp.com/api/',
+  envName: 'production',
+  gtagId: 'G-JWVLF041Q4'
 };
