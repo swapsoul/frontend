e46e90608fb3f2b5c5b2fb827060d6401a8687b0
Rama Teja <gandhamramateja@gmail.com>
2021-05-09 12:16:06 +0530

    PriceFilter

--- src/app/routes/filter.pipe.ts
+++ src/app/routes/filter.pipe.ts
@@ -5,24 +5,44 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, sName: string,maxp: string,minp: string,dis: string): any {
-    let comp:number = 0;
-    let compmin: number = 0;
+  transform(value: any, maxp: string[]): any {
+    //let comp:number = 0;
+    //let compmin: number = 0;
     let maxprice: number = Number.MAX_SAFE_INTEGER;
     let minprice: number = 0;
-    let discount: number = 0;
-    console.log(comp);
-    let maxlen = maxp.length;
+    let left: number;
+    let right: number = Number.MAX_SAFE_INTEGER;
+    //let discount: number = 0;
+    //let maxlen = maxp.length;
     
-    if(sName=="" && maxp=="" && minp=="" && dis==""){
+    if(maxp.length==0){
       return value;
     }
 
-    if (maxp != "" && maxp[maxlen-1]!='+') {
-      maxprice = parseInt(maxp);
-    }
+    const prodArray: any[] = [];
 
-    if (minp != "") {
+    for(let range of maxp)
+    {
+      if(range[range.length-1]!='+'){
+      left = parseInt(range.substr(0,range.indexOf('-')));
+      right = parseInt(range.substr(range.indexOf('-')+1,range.length-1));
+      }
+      else{
+        left = parseInt(range.substr(0,range.indexOf('+')));
+        right = Number.MAX_SAFE_INTEGER;
+      }
+      for(let i=0;i<value.length;i++)
+      {
+        let prodPrice: any = value[i].productSalePrice;
+        if (prodPrice >= left && prodPrice <= right) {
+          prodArray.push(value[i]);
+        }
+      }
+    }
+    return prodArray;
+
+
+    /*if (minp != "") {
       minprice = parseInt(minp);
     }
 
@@ -36,18 +56,18 @@ export class FilterPipe implements PipeTransform {
 
     console.log(comp);
 
-    const prodArray: any[]=[];
+    
     for(let i=0;i<value.length;i++)
     {
-      let prodRat:any=value[i].productRating;
+      //let prodRat:any=value[i].productRating;
       let prodPrice:any=value[i].productSalePrice;
-      let prodDis:any = value[i].productDiscount;
-      if(prodRat>=comp && prodPrice<=maxprice && prodPrice>=minprice && prodDis>=discount)
+      //let prodDis:any = value[i].productDiscount;
+      if(prodPrice<=maxprice)
       {
         prodArray.push(value[i]);
       }
     }
-    return prodArray;
+    return prodArray;*/
   }
 
 }
--- src/app/routes/men/men.component.ts
+++ src/app/routes/men/men.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { GlobalService } from 'src/app/services/global/global.service';
+import { FilterPipe } from 'src/app/routes/filter.pipe';
 
 @Component({
   selector: 'app-men',
@@ -16,15 +17,16 @@ export class MenComponent implements OnInit {
   maxi = new FormControl();
   mini = new FormControl();
   disc = new FormControl();
+  filt = new FilterPipe();
   filteredProducts: any[];
   rating:string="";
-  maxp:string="";
+  maxp:string[] = [];
   minp:string="";
   dis:string="";
 
   toppingList: string[] = ['Discount - High to Low', 'Price- Low to High', 'Price- High to Low', 'Newest First', 'Rating'];
   ratingList: string[] = ['4* & above','3* & above','2* & above','1* & above'];
-  maxplist: string[] = ['250','500','1000','1500','2000','2500','2500+'];
+  maxplist: string[] = ['300-500','500-700','700+'];
   minplist: string[] = ['250', '500', '1000', '1500', '2000', '2500'];
   disclist: string[] = ['10% or more', '20% or more', '30% or more', '40% or more', '50% or more', '60% or more', '70% or more', '80% or more', '90% or more'];
 
@@ -44,6 +46,7 @@ export class MenComponent implements OnInit {
   }
 
   sort(event): void {
+    this.products = this.filt.transform(this.products,this.maxp);
     if (event.value === this.toppingList[0]) {
       this.products.sort((a, b) => (a.productDiscount < b.productDiscount) ? 1 : -1);
     } else if (event.value === this.toppingList[1]) {
@@ -72,9 +75,4 @@ export class MenComponent implements OnInit {
   discount(event){
     this.dis = event.value;
   }
-
-  updateData(data){
-    console.log(data);
-  }
-
 }
